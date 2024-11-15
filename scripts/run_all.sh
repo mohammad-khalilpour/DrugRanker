1028a073f6450d4bb32486d0dbd14ba29d3f9289
Taha Mohammadzadeh <taha.mohammadzadeh.v@gmail.com>
2024-11-15 13:38:51 +0330

    add relative path

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
-DATA_FOLDER="/Users/tahamohammadzadeh/PycharmProjects/DrugRanker/DrugRanker/data/ctrp"
-ae_path="/Users/tahamohammadzadeh/PycharmProjects/DrugRanker/DrugRanker/expts/ae/LCO/ctrp/all_bs_64_outd_128/fold_2/model.pt"
-genexp_path='/Users/tahamohammadzadeh/PycharmProjects/DrugRanker/DrugRanker/data/CCLE/CCLE_expression.csv'
-models=("listone" "listall" "pairpushc" "lambdaloss" "lambdarank" "neuralndcg")
+DATA_FOLDER="data/ctrp"
+ae_path="expts/ae/LCO/ctrp/all_bs_64_outd_128/fold_2/model.pt"
+genexp_path='data/CCLE/CCLE_expression.csv'
+models=("pairpushc" "lambdaloss" "lambdarank" "neuralndcg" "listone" "listall")
 
 representations=('morgan_count' 'map4' 'avalon' 'atom_pair' '2d_pharmacophore' 'layered_rdkit')
 
 for model in "${models[@]}"; do
     for representation in "${representations[@]}"; do
+        ls
         python src/cross_validate.py \
             --model "$model" \
             --data_path "$DATA_FOLDER/LCO/aucs.txt" \
@@ -17,7 +17,8 @@ for model in "${models[@]}"; do
             --pretrained_ae \
             --trained_ae_path "$ae_path" \
             --feature_gen "$representation" \
-            --genexp_path "$genexp_path"
+            --max_iter "2" \
+            --genexp_path "$genexp_path" \
             --setup "LCO"
     done
 done
