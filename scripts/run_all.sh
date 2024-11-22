c254023b1d9c482568688c24bed362affa18a45a
Sarmeili <sarmeili.f@gmail.com>
2024-11-22 04:55:17 +0330

    log naming_fixed

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -10,14 +10,14 @@ save_path="expts/result/LCO/$DATA_SET/"
 # representations=('morgan_count' 'avalon' 'atom_pair' '2d_pharmacophore' 'layered_rdkit')
 # setups=("LCO" "LRO")
 
-models=("neuralndcg")
-representations=("layered_rdkit")
+models=("lambdaloss" "lambdarank" "neuralndcg")
+representations=("atom_pair")
 setups=("LCO")
 
 for setup in "${setups[@]}"; do
     for model in "${models[@]}"; do
         for representation in "${representations[@]}"; do
-            for fold in $(seq 0 0); do
+            for fold in $(seq 0 4); do
                 ae_path="expts/ae/LCO/$DATA_SET/all_bs_64_outd_128/fold_$fold/model.pt"
                 save_dir="expts/result/$setups/$DATA_SET/$model/$representation/"
                 log_dir=$save_dir/logs/
@@ -32,10 +32,10 @@ for setup in "${setups[@]}"; do
                     --pretrained_ae \
                     --trained_ae_path "$ae_path" \
                     --feature_gen "$representation" \
-                    --max_iter "1" \
+                    --max_iter 100 \
                     --genexp_path "$genexp_path" \
                     --setup "$setup" \
-                    --log_steps 1 > $log_dir/avg_results.txt
+                    --log_steps 5 > $log_dir/results_$((fold+1)).txt
             done
         done
     done
