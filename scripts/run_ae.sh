2876aabcbbaae3e129cb62c045cd7e9cce7a1d19
Sarmeili <sarmeili.f@gmail.com>
2024-11-23 16:27:38 +0330

    LRO fixed

--- scripts/run_ae.sh
+++ scripts/run_ae.sh
@@ -13,20 +13,22 @@ if [[ $data == 'ctrpv2' ]]; then
 fi
 
 # separate pretrained model for each fold in LCO experiments
-for fold in $(seq 0 4); do
-for bs in ${batch[@]}; do
-	for outd in ${outdim[@]}; do
-		save_dir="${expt_dir}/all_bs_${bs}_outd_${outd}/fold_${fold}/"
-		mkdir -p $save_dir
-		python3 src/train_ae.py --genexp_file $gexp_file --splits_path data/${data}/LCO/pletorg/fold_${fold}/ \
-		--save_path $save_dir --ae_out_size $outd --bs $bs --cuda > $save_dir/train.log
-	done
-done
-done
+# for fold in $(seq 0 4); do
+# for bs in ${batch[@]}; do
+# 	for outd in ${outdim[@]}; do
+# 		save_dir="${expt_dir}/all_bs_${bs}_outd_${outd}/fold_${fold}/"
+# 		mkdir -p $save_dir
+# 		python3 src/train_ae.py --genexp_file $gexp_file --splits_path data/${data}/LCO/pletorg/fold_${fold}/ \
+# 		--save_path $save_dir --ae_out_size $outd --bs $bs --cuda > $save_dir/train.log
+# 	done
+# done
+# done
 
 
 # to pretrain single GeneAE model using all cell lines in LRO experiments
-#expt_dir="/content/DrugRank/expts/ae/LRO/${data}/"
-#save_dir="${expt_dir}/all_bs_${bs}_outd_${outd}/"
-#mkdir -p $save_dir
-#python src/train_ae.py --genexp_file $gexp_file --save_path $save_dir --ae_out_size $outd --bs $bs --use_all --cuda > $save_dir/train.log
+outd=128
+bs=64
+expt_dir="expts/ae/LRO/${data}"
+save_dir="${expt_dir}/all_bs_${bs}_outd_${outd}/"
+mkdir -p $save_dir
+python3 src/train_ae.py --genexp_file $gexp_file --save_path $save_dir --ae_out_size $outd --bs $bs --use_all --cuda > $save_dir/train.log
--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,37 +1,43 @@
 #!/bin/bash
 
 
-DATA_FOLDER="data/ctrp"
-DATA_SET="ctrp"
+DATA_FOLDER="data/prism"
+DATA_SET="prism"
 genexp_path='data/CCLE/CCLE_expression.csv'
 save_path="expts/result/LCO/$DATA_SET/"
-log_steps=1
-max_iter=2
-num_folds=2
+log_steps=5
+max_iter=100
+num_folds=5
 device=${1:-'cuda:0'}
 
 # models=("pairpushc" "lambdaloss" "lambdarank" "neuralndcg" "listone" "listall")
 # representations=('morgan_count' 'avalon' 'atom_pair' '2d_pharmacophore' 'layered_rdkit')
 # setups=("LCO" "LRO")
 
-models=("listone")
-representations=("atom_pair")
-setups=("LCO")
+models=("lambdaloss" "lambdarank")
+representations=("morgan_count")
+setups=("LRO")
 
 for setup in "${setups[@]}"; do
     for model in "${models[@]}"; do
         for representation in "${representations[@]}"; do
             for fold in $(seq 0 $((num_folds-1))); do
-                ae_path="expts/ae/LCO/$DATA_SET/all_bs_64_outd_128/fold_$fold/model.pt"
-                save_dir="expts/result/$setups/$DATA_SET/$model/$representation/"
+                if [[ $setup == 'LCO' ]]; then
+                    ae_path="expts/ae/$setup/$DATA_SET/all_bs_64_outd_128/fold_$fold/model.pt"
+                    splits_path="$DATA_FOLDER/$setup/pletorg/"
+                elif [[ $setup == 'LRO' ]]; then
+                    ae_path="expts/ae/$setup/$DATA_SET/all_bs_64_outd_128/model.pt"
+                    splits_path="$DATA_FOLDER/$setup/"
+                fi
+                save_dir="expts/result/$setup/$DATA_SET/$model/$representation/"
                 log_dir=$save_dir/logs/
                 mkdir -p $log_dir
                 python3 src/cross_validate.py \
                     --model "$model" \
                     --only_fold $fold \
-                    --data_path "$DATA_FOLDER/LCO/aucs.txt" \
+                    --data_path "$DATA_FOLDER/$setup/aucs.txt" \
                     --smiles_path "$DATA_FOLDER/cmpd_smiles.txt" \
-                    --splits_path "$DATA_FOLDER/LCO/pletorg/" \
+                    --splits_path $splits_path \
                     --save_path $save_dir \
                     --pretrained_ae \
                     --trained_ae_path "$ae_path" \
